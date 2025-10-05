1650eb07d7ce732646ff37c5de2e2176d4baad0e
Only-One <107347425+Digital1ID@users.noreply.github.com>
2025-10-05 12:06:11 +0700

    Create index.php

---
+++ p2p/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8"/>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>037HD</title>
+        <link href="https://www.stream1689.com/v4/css/analogy-dark.css?id=f068ab4c27858eae2a3c" rel="stylesheet" type="text/css">
+        <link href="https://www.stream1689.com/v4/css/six.css?id=f068ab4c27858eae2a3c" rel="stylesheet" type="text/css">
+        <style type="text/css">
+            .embed,body {
+                width: 100%!important;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="app"></div>
+        <script src="https://ssl.p.jwpcdn.com/player/v/7.12.13/jwplayer.js" type="23222e9c31f69ddb092dbba4-text/javascript"></script>
+        <script src="https://ssl.p.jwpcdn.com/player/v/7.12.13/provider.hlsjs.js" type="23222e9c31f69ddb092dbba4-text/javascript"></script>
+        <script type="23222e9c31f69ddb092dbba4-text/javascript">
+            jwplayer.key="itWTHIhJdAIEfyNdzc0HeaZWvkzjhvdQU3VtAw==";
+        </script>
+        <script src="https://www.stream1689.com/v4/js/stream1688/manifest.js" type="23222e9c31f69ddb092dbba4-text/javascript"></script>
+        <script src="https://www.stream1689.com/v4/js/stream1688/vendor.js" type="23222e9c31f69ddb092dbba4-text/javascript"></script>
+        <script src="https://www.stream1689.com/v4/js/stream1688/atomic.js" type="23222e9c31f69ddb092dbba4-text/javascript"></script>
+        <script type="23222e9c31f69ddb092dbba4-text/javascript">
+            atomic({ "playlist": [{ "id": "1089236258", "label": "พากย์ไทย", "api": "https://www.stream1689.com/api/analogy/media/901763?expires=1089236258&token=0269b2e943cca1480c870d4df3d4997f&signature=r63q7dw2rl77l8pfh28n33e150ejqe2gbwwt8lkq5bez0sac37", "token": "0269b2e943cca1480c870d4df3d4997f" } ,{ "id": "1089236258", "label": "Soundtrack ซับ", "label": "ซับไทย", "api": "https://www.stream1689.com/api/analogy/media/907432?expires=1089236258&token=0269b2e943cca1480c870d4df3d4997f&signature=r63q7dw2rl77l8pfh28n33e150ejqe2gbwwt8lkq5bez0sac37", "token": "0269b2e943cca1480c870d4df3d4997f" } ], "options": { "title": "u0032\u0079\u006f\u0068\u0064", "height": "560", "maxErrors": "1", "requestOptions": { "has_ipv6": "0" }, "poster": "https://www.v8movie.com/playerv8-2.png", "ads": [ { "video": "https://www.upfastfile.com/upload/1xbet.mp4", "link": "https://bit.ly/3Xp57Lg", "linkButtonText": "\u0e2a\u0e21\u0e31\u0e04\u0e23\u0e2a\u0e21\u0e32\u0e0a\u0e34\u0e01", "canSkipIn": 3, "skip": 3 }] } }).$mount('#app');
+        </script>
+        <script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="23222e9c31f69ddb092dbba4-|49" defer></script>
+    </body>
+</html>
