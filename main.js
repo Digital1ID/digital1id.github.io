dc0a02dec254b3a0156019e141e694bcb0ec151e
Only-One <107347425+Digital1ID@users.noreply.github.com>
2026-02-13 14:40:04 +0700

    Update main.js
    Signed-off-by: Only-One <107347425+Digital1ID@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 const channels = [
-               {
+              { 
+                name: "5HD",
+                sources: [
+                    { 
+                        name: "THAI", 
+                        url: "https://cri-streamer3.cdn.3bbtv.com:8443/3bb/live/5/5.mpd",
+                        type: "dash",
+                        drm: {
+                            kid: "87db4361f7894655a4656e9c8b935a02",
+                            key: "b025a4b950df41158a87cfc8d6f2ac34"
+                        }
+                    }
+                ],
+                img: "https://ais-s.ais-vidnt.com/ais/play/origin/LIVE/channelicon/ch5new.logo.png", 
+                category: "Digital TV",
+                package: ["150","250","500"],
+                description: "THAI News"
+              },
+              {
                 name: "BBC News", 
                 sources: [
                     { name: "UK", url: "https://tplay.live/out/news/bbc-news/index.m3u8" },
