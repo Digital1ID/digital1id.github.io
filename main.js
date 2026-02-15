3dc4b6b2c5df719a7fcc6282477b58bc6ba005a9
Only-One <107347425+Digital1ID@users.noreply.github.com>
2026-02-15 08:47:51 +0700

    update main.js

--- main.js
+++ main.js
@@ -396,7 +396,7 @@ const channels = [
                 package: ["150","250","500"],
                 description: "THAI"
               },{ 
-                name: "",
+                name: "CHANNEL 8",
                 sources: [
                     { name: "Server 1", url: "http://54c241893bf39.streamlock.net:1935/ch8/ch8/chunklist.m3u8"},
                     { 
