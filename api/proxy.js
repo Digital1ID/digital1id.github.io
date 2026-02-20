e7634fdfe4ea3f73841e85b58e9c53a3b2b2ae6a
Only-One <107347425+Digital1ID@users.noreply.github.com>
2026-02-20 22:35:59 +0700

    update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     const response = await fetch(target, {
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-        "Referer": "https://www.xn--72c9ab1ec1bc6q.online/"
+        "Referer": "https://357ms.com/"
       }
     });
 
